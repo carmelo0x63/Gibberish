dbc321ba24a1ed4f9383748b7d17ceea45f2ca3d
Carmelo Califano <ccalifan@cisco.com>
2019-10-09 20:59:19 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Gibberish
-Silly project about fetching random sentences (verb + adjective + noun) from a SQL database and publishing them as JSON. RESTful API based on Python + Flask.
+Silly project about fetching random sentences (verb + adjective + noun) from a SQL database and publishing them as JSON.<br/>
+RESTful API based on Python + Flask.
---
+++ makebabble_standalone.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# description: generate random gibberish resembling business-like talk.
+#              Very useful during meetings and interviews.
+# author: ccalifan@cisco.com
+# date (ISO 8601): 2016-03-09
+
+# Import some modules
+from __future__ import print_function	# print() as a function not as a statement
+import argparse	# Parser for command-line options, arguments and sub-commands
+import random	# Generate pseudo-random numbers
+import sqlite3	# DB-API 2.0 interface for SQLite databases
+from flask import Flask, jsonify, make_response
+
+# Global variables
+dbName = 'gibberish.db'
+tables = ['adjectives', 'nouns', 'verbs']
+itemsCount = {'numA':0, 'numN':0, 'numV':0}
+
+def main():
+    conn = sqlite3.connect(dbName)
+    c = conn.cursor()
+    for t, i in zip(tables, sorted(itemsCount.keys())):
+      c.execute('SELECT count(*) FROM {table};'.format(table=t))
+      itemsCount[i] = c.fetchone()[0]
+
+    getA = random.randrange(itemsCount['numA']) + 1
+    getN = random.randrange(itemsCount['numN']) + 1
+    getV = random.randrange(itemsCount['numV']) + 1
+
+    c.execute('SELECT adj FROM adjectives WHERE id = {id};'.format(id=getA))
+    print(itemsCount['numA'], getA, c.fetchone())
+    c.execute('SELECT noun FROM nouns WHERE id = {id};'.format(id=getN))
+    print(itemsCount['numN'], getN, c.fetchone())
+    c.execute('SELECT verb FROM verbs WHERE id = {id};'.format(id=getV))
+    print(itemsCount['numV'], getV, c.fetchone())
+
+    conn.close()
+
+if __name__ == '__main__':
+	main()
